176d3aa19ec3dd781064ff96a214f23a8d9c418f
Peng Tsinv <32813194+TuringCodeOne@users.noreply.github.com>
2022-05-19 11:18:40 +0800

    feat: 增加网站前缀

--- blog/.vuepress/config.ts
+++ blog/.vuepress/config.ts
@@ -1,12 +1,14 @@
 import { defineUserConfig } from "vuepress";
 import theme from "./theme";
 
+const userBase = 'blog';
+
 export default defineUserConfig({
   lang: "zh-CN",
   title: "主题演示",
   description: "vuepress-theme-hope 的演示",
 
-  base: "/",
+  base: `/${userBase}/`,
 
   head: [
     [
